c29dbd8dab65b358547521bafb0fa90db0504c58
render1980 <render1980@gmail.com>
2021-01-14 13:56:55 +0300

    + draw functions draft

--- main.js
+++ main.js
@@ -8,9 +8,15 @@ var cubeValue = 0;
 var canvas = document.getElementById("cheesyCanvas");
 var ctx = canvas.getContext("2d");
 
-prepareTable();
-console.log(pantry);
-console.log(deck);
+main();
+
+function main() {
+  prepareTable();
+  console.log(pantry);
+  console.log(deck);
+  
+  drawDeck();
+}
 
 function prepareTable() {
   var cards = shuffle();
@@ -18,4 +24,31 @@ function prepareTable() {
   pantry = cards.slice(0, 6);
   // others -> Deck
   deck = cards.slice(6, cards.length);
+}
+
+function drawCard(x, y) {
+  ctx.beginPath();
+  ctx.rect(x, y, 40, 50);
+  ctx.fillStyle = "#FF0000";
+  // TODO: + border
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawPantry() {
+
+}
+
+function drawDeck() {
+  drawCard(40, 50);
+  drawCard(45, 55);
+}
+
+function drawCube() {
+  ctx.beginPath();
+  ctx.rect(x, y, 20, 20);
+  ctx.fillStyle = "#FFFFFF";
+  // TODO: + cube points
+  ctx.fill();
+  ctx.closePath();  
 }
