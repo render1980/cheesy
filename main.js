9910aadeb9f7cf52be90c368d6e6709141c44990
render1980 <render1980@gmail.com>
2021-01-27 12:02:30 +0300

    + on click cube => redraw it

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 import { getRandomCubeNumber, shuffle } from "./mechanics.js";
-
-const PANTRY_SIZE = 6;
-const CUBE_X = 10;
-const CUBE_Y = 150;
-const CUBE_WIDTH = 40;
-const CUBE_LENGTH = 40;
-const CARD_WIDTH = 60;
-const CARD_LENGTH = 80;
-
+// Table objects
 var pantry = [];
 var deck = [];
 var discard = [];
@@ -15,6 +7,16 @@ var cubeValue = 0;
 
 var canvas = document.getElementById("cheesyCanvas");
 var ctx = canvas.getContext("2d");
+// objects parameters
+const PANTRY_SIZE = 6;
+const CUBE_X = 10;
+const CUBE_Y = 150;
+const CUBE_WIDTH = 40;
+const CUBE_LENGTH = 40;
+const CARD_WIDTH = 60;
+const CARD_LENGTH = 80;
+// states
+var cubePressed = false;
 
 main();
 
@@ -22,10 +24,17 @@ function main() {
   prepareTable();
   console.log(pantry);
   console.log(deck);
-  var cubeNumber = getRandomCubeNumber();
   drawDeck();
+
+  var cubeNumber = getRandomCubeNumber();
   drawCube(cubeNumber);
+
   drawPantry();
+  addEventListeners();
+}
+
+function addEventListeners() {
+  document.addEventListener("mousedown", onCubeMouseClick, false);
 }
 
 function prepareTable() {
@@ -34,6 +43,25 @@ function prepareTable() {
   pantry = cards.slice(0, 6);
   // others -> Deck
   deck = cards.slice(6, cards.length);
+}
+
+function drawDeck() {
+  const DECK_X = 10;
+  const DECK_Y = 10;
+  const DECK_OFFSET = 5;
+  drawCard(DECK_X, DECK_Y, getRandomCubeNumber());
+  drawCard(DECK_X + DECK_OFFSET, DECK_Y + DECK_OFFSET, getRandomCubeNumber());
+}
+
+function drawCube(num) {
+  ctx.clearRect(CUBE_X, CUBE_Y, CUBE_WIDTH, CUBE_LENGTH);
+  var img = new Image(CUBE_WIDTH, CUBE_LENGTH);
+  img.onload = function () {
+    ctx.drawImage(img, CUBE_X, CUBE_Y);
+    ctx.beginPath();
+    ctx.stroke();
+  };
+  img.src = "./images/cube" + num + ".png";
 }
 
 function drawPantry() {
@@ -47,24 +75,6 @@ function drawPantry() {
   }
 }
 
-function drawDeck() {
-  const DECK_X = 10;
-  const DECK_Y = 10;
-  const DECK_OFFSET = 5;
-  drawCard(DECK_X, DECK_Y, getRandomCubeNumber());
-  drawCard(DECK_X + DECK_OFFSET, DECK_Y + DECK_OFFSET, getRandomCubeNumber());
-}
-
-function drawCube(num) {
-  var img = new Image(CUBE_WIDTH, CUBE_LENGTH);
-  img.onload = function () {
-    ctx.drawImage(img, CUBE_X, CUBE_Y);
-    ctx.beginPath();
-    ctx.stroke();
-  };
-  img.src = "./images/cube" + num + ".png";
-}
-
 function drawCard(x, y, num) {
   var img = new Image(CARD_WIDTH, CARD_LENGTH);
   img.onload = function () {
@@ -73,4 +83,10 @@ function drawCard(x, y, num) {
     ctx.stroke();
   };
   img.src = "./images/card" + num + ".png";
+}
+
+function onCubeMouseClick(e) {
+  cubePressed = true;
+  var cubeNumber = getRandomCubeNumber();
+  drawCube(cubeNumber);
 }
